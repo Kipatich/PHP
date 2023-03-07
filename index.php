ce1647af1bea25d7cced10bcb03662b05fbdefe0
Kopatich <sergeytula04@gmail.com>
2023-03-07 11:39:16 +0300

    made html

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <img class="header_logo" src="/img/1.jpg" alt="logo">
+            <p class="header_text">Домашняя работа: Hello, World!</p>
+        </div>
+
+    </header>
+
+
+    <main>
+        <?php echo 'Привет мир!'; ?>
+    </main>
+
+
+    <footer>
+        <?php echo 'Привет мир!'; ?>
+    </footer>
+</body>`
